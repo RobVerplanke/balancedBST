2e87a7c85a2f163bb8550a997c22821cf0ebb58f
RobVerplanke <rverplanke@gmail.com>
2023-09-21 14:46:40 +0200

    create function to remove duplicates from array

--- bst/tree.js
+++ bst/tree.js
@@ -1,9 +1,14 @@
+// Remove duplicated elements
+function removeDuplicates(arr) {
+  return arr.filter((element, index) => arr.indexOf(element) === index);
+}
+
 export default class BinaryTree {
   constructor(array) {
     this.root = null;
   }
 
-  buildTree(array) {
+  buildTree(arr) {
 
   }
 
